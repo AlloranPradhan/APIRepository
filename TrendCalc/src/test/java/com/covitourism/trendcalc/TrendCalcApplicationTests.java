7e233bb8c4a5f38ee2463aa93a27d2a88f97d219
AlloranPradhan <87431816+AlloranPradhan@users.noreply.github.com>
2021-08-09 22:55:36 +0530

    Create TrendCalcApplicationTests.java

---
+++ TrendCalc/src/test/java/com/covitourism/trendcalc/TrendCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covitourism.trendCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9de6fdcdd3ef63827c4716c97760c6f400b874f3
AlloranPradhan <87431816+AlloranPradhan@users.noreply.github.com>
2021-08-10 11:24:01 +0530

    Add files via upload

---
+++ SpringSecurity/src/main/java/com/spring/security/SpringSecurityApplication.java
@@ -0,0 +1,27 @@
+package com.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+
+public class SpringSecurityApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedHeaders("*").allowedOriginPatterns("*").allowedMethods("*")
+				.allowCredentials(true);
+			}
+		};
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
